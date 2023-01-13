2a09eb82c1363037091e18c11893a98259c317a7
Danil <rusbeks@outlook.com>
2023-01-13 14:38:37 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using RSA;
+
+var rsa = new Rsa();
+
+int p, q;
+Console.WriteLine("Выберите вариант задания p, q: (1 - в ручную, 2 - автоматически)");
+switch (Console.ReadLine()![0])
+{
+    case '1':
+        Console.WriteLine("Введите p: ");
+        p = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите q: ");
+        q = int.Parse(Console.ReadLine());
+        break;
+    case '2':
+        var primes = rsa.GeneratePrimes();
+        p = primes[0];
+        q = primes[1];
+        break;
+    default:
+        throw new Exception("Введены некорректные данные");
+}
+
+int n = p * q;
+int m = (p - 1) * (q - 1);
+int e = rsa.GenerateEncryptor(m);
+int d = rsa.GenerateDecryptor(e, m);
+
+bool exit = false;
+do
+{
+    Console.WriteLine("Введите строку: ");
+    string input = Console.ReadLine()!;
+    Console.WriteLine("Что вы хотите сделать? (1 - зашифровать, 2 - расшифровать, q - выход): ");
+    switch (Console.ReadLine()![0])
+    {
+        case '1':
+            var encrypted = rsa.Encrypt(input, e, n);
+            Console.WriteLine($"Зашифрованное сообщение: {encrypted}");
+            break;
+        case '2':
+            var decrypted = rsa.Decrypt(input, d, n);
+            Console.WriteLine($"Расшифрованное сообщение: {decrypted}");
+            break;
+        case 'q':
+            exit = true;
+            break;
+    }
+} while (!exit);
---
+++ Rsa.cs
@@ -0,0 +1,85 @@
+﻿using System.Numerics;
+using System.Text;
+
+namespace RSA;
+
+public class Rsa
+{
+    private const int MinimalPrime = 100;
+    private const int MaximalPrime = 5000;
+
+    public int[] GeneratePrimes()
+    {
+        var isPrime = new bool[MaximalPrime + 1];
+        for (int i = 0; i <= MaximalPrime; i++) 
+            isPrime[i] = true;
+
+        for (int i = 2; i <= MaximalPrime; i++)
+        {
+            if (!isPrime[i]) continue;
+            
+            for (int j = 2 * i; j <= MaximalPrime; j += i) 
+                isPrime[j] = false;
+        }
+        
+        var primes = new List<int>();
+        for (int i = MinimalPrime; i <= MaximalPrime; i++)
+        {
+            if (isPrime[i])
+            {
+                primes.Add(i);
+            }
+        }
+        var rand = new Random();
+        int first = rand.Next() % primes.Count / 2;
+        int second = rand.Next() % primes.Count / 2 + primes.Count / 2;
+        return new[] { primes[first], primes[second] };
+    }
+
+    public string Encrypt(string m, int e, int n)
+    {
+        var encodedMessage = new StringBuilder();
+        foreach (char c in m)
+        {
+            BigInteger k = BigInteger.ModPow(c, e, n);
+            encodedMessage.Append(k + " ");
+        }
+        return encodedMessage.ToString();
+    }
+
+    public string Decrypt(string input, int d, int n)
+    {
+        var result = new StringBuilder();
+        var arr = input.Split(' ');
+        foreach (string item in arr)
+        {
+            if(string.IsNullOrEmpty(item)) continue;
+            
+            BigInteger k = BigInteger.ModPow(BigInteger.Parse(item), d, n);
+            result.Append((char)k);
+        }
+        return result.ToString();
+    }
+
+    public int GenerateEncryptor(int m)
+    {
+        for (int e = 10; e < m; e++)
+        {
+            if (BigInteger.GreatestCommonDivisor(m, e) == 1) return e;
+        }
+
+        return m - 1;
+    }
+
+    public int GenerateDecryptor(int e, int m)
+    {
+        //int d = (int)BigInteger.ModPow(e, m - 2, m);
+
+        for (int X = 1; X < m; X++)
+            if (((e % m) * (X % m)) % m == 1)
+                return X;
+        return 1;
+        
+        //return d;
+    }
+}
